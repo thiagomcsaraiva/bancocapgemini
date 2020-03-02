e3b3710df98102853ac882b747e24bcd83df770f
Thiago Saraiva <thiagomcsaraiva@gmail.com>
2020-03-02 00:35:06 -0300

    Finalização do Front

--- front/src/components/conta/Home.vue
+++ front/src/components/conta/Home.vue
@@ -1,104 +1,98 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-        <v-list-item>
-            <v-list-item-content>
-                <v-list-item-title>CONTAS</v-list-item-title>
-                <v-list-item-subtitle>Clique para acessar a conta</v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-        <!-- LISTA DE CONTAS CADASTRADAS -->
-        <v-list>
-          <v-list-item
-            v-for="item in contas"
-            :key="item.id"
-            link
-            @click="detalharConta(item)"
-          >
-            <v-list-item-avatar>
-              <img :src="`https://randomuser.me/api/portraits/men/${item.id}.jpg`" alt="">
-            </v-list-item-avatar>
-            <v-list-item-title v-text="item.nome_titular" />
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app clipped-left color="blue" dense>    
-        <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-                <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-on="on"/>
-            </template>
-            <span v-if="!drawer">Mostrar Contas</span>
-            <span v-else>Ocultar Contas</span>
-        </v-tooltip>
-        <v-toolbar-title class="mr-12 align-center">
-        <span class="title">Banco Capgemini</span>
-        </v-toolbar-title>
-    </v-app-bar>
-
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row justify="center" align="center">
-          <v-col cols="12" sm="8" md="6">
-            <v-alert :type="tipoMsg" v-model="hasMsg" dismissible>
-                {{ msg }}
-            </v-alert>
-            <v-card class="elevation-12" v-if="conta.numero && drawer">
-            
-                <!-- SESSÃO DAS INFORMAÇÕES DA CONTA -->
-                <v-list-item three-line>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app clipped>
+            <v-list dense>
+                <v-list-item>
                     <v-list-item-content>
-                        <div class="mb-4">Conta: {{ conta.numero }}</div>
-                        <div class="mb-4">Nome do titular: {{ conta.nome_titular }}</div>
-                        <v-list-item-title class="headline mb-1">Saldo: R$ {{ formataValor(conta.saldo) }}</v-list-item-title>
+                        <v-list-item-title>CONTAS</v-list-item-title>
+                        <v-list-item-subtitle>Clique para acessar a conta</v-list-item-subtitle>
                     </v-list-item-content>
-
-                    <v-list-item-avatar>
-                        <img :src="`https://randomuser.me/api/portraits/men/${conta.numero}.jpg`" alt="">
-                    </v-list-item-avatar>
                 </v-list-item>
 
                 <v-divider></v-divider>
+                <!-- LISTA DE CONTAS CADASTRADAS -->
+                <v-list>
+                    <v-list-item v-for="item in contas" :key="item.id" link @click="detalharConta(item)">
+                        <v-list-item-avatar>
+                            <img :src="`https://randomuser.me/api/portraits/men/${item.id}.jpg`" alt="">
+                        </v-list-item-avatar>
+                        <v-list-item-title v-text="item.nome_titular" />
+                    </v-list-item>
+                </v-list>
+                <v-divider></v-divider>
+            </v-list>
+        </v-navigation-drawer>
 
-                <!-- SESSÃO DAS AÇÕES DE DEPÓSITO E SAQUE -->
-                <v-card-actions v-if="!operacao">
-                    <v-btn dark depressed color="teal" @click="operacao = 'depósito', hasMsg = false">
-                        Depósito
-                    </v-btn>
-                    <v-btn v-if="conta.saldo > 0" dark depressed color="error" @click="operacao = 'saque', hasMsg = false">
-                        Saque
-                    </v-btn>
-                </v-card-actions>
-                <!-- SESSÃO DO FORM DE DEPÓSITO E SAQUE -->
-                <div class="px-2 py-2" v-if="operacao">
-                    <v-form ref="form" azy-validation>
-                        <v-row>
-                            <v-col md="4">
-                                <vuetify-money v-model="valor" label="Valor" outlined :options="options"/>
-                            </v-col>
-                            <v-col md="8">
-                                <v-btn :disabled="valor <= 0" color="success" class="mr-4 mt-3" @click="realizarOperacao()">Confirmar {{operacao}}</v-btn>
-                                <v-btn class="mr-4 mt-3" @click="voltar()">Voltar</v-btn>
-                            </v-col>
-                        </v-row>
-                    </v-form>
-                </div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+        <v-app-bar app clipped-left color="blue" dense>    
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-on="on"/>
+                </template>
+                <span v-if="!drawer">Mostrar Contas</span>
+                <span v-else>Ocultar Contas</span>
+            </v-tooltip>
+            <v-toolbar-title class="mr-12 align-center">
+                <span class="title">Banco Capgemini</span>
+            </v-toolbar-title>
+        </v-app-bar>
+
+        <v-container fluid>
+            <v-row justify="center" align="center">
+                <v-col cols="12" sm="8" md="6">
+                    <v-alert :type="tipoMsg" v-model="hasMsg" dismissible>
+                        {{ msg }}
+                    </v-alert>
+                    <v-card class="elevation-12" v-if="conta.numero && drawer">
+                    
+                        <!-- SESSÃO DAS INFORMAÇÕES DA CONTA -->
+                        <v-list-item three-line>
+                            <v-list-item-content>
+                                <div class="mb-4">Conta: {{ conta.numero }}</div>
+                                <div class="mb-4">Nome do titular: {{ conta.nome_titular }}</div>
+                                <v-list-item-title class="headline mb-1">Saldo: R$ {{ conta.saldo | formataValor }}</v-list-item-title>
+                            </v-list-item-content>
+
+                            <v-list-item-avatar>
+                                <img :src="`https://randomuser.me/api/portraits/men/${conta.numero}.jpg`" alt="">
+                            </v-list-item-avatar>
+                        </v-list-item>
+
+                        <v-divider></v-divider>
+
+                        <!-- SESSÃO DAS AÇÕES DE DEPÓSITO E SAQUE -->
+                        <v-card-actions v-if="!operacao">
+                            <v-btn dark large color="teal" @click="operacao = 'depósito', hasMsg = false">
+                                Depósito
+                            </v-btn>
+                            <v-btn v-if="conta.saldo > 0" dark large color="error" @click="operacao = 'saque', hasMsg = false">
+                                Saque
+                            </v-btn>
+                        </v-card-actions>
+                        <!-- SESSÃO DO FORM DE DEPÓSITO E SAQUE -->
+                        <div class="px-2 py-2" v-if="operacao">
+                            <v-form ref="form" azy-validation>
+                                <v-row>
+                                    <v-col md="4">
+                                        <vuetify-money v-model="valor" label="Valor" outlined :options="options"/>
+                                    </v-col>
+                                    <v-col md="8">
+                                        <v-btn large :disabled="valor <= 0" color="success" class="mr-4 mt-1" @click="realizarOperacao()">Confirmar {{operacao}}</v-btn>
+                                        <v-btn large class="mr-4 mt-1" @click="voltar()">Voltar</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-form>
+                        </div>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
   
 </template>
 
 <script>
     import Conta from './service.js';
+
     export default {
         name: 'Home',
         data: () => ({
@@ -141,6 +135,7 @@
             voltar(){
                 this.operacao = null;
                 this.valor = null;
+                this.hasMsg = false;
             },
             // REALIZA DEPÓSITO OU SAQUE
             realizarOperacao(){
@@ -166,12 +161,15 @@
                         });
                     }
                 });
-            },
-            // FORMATA O SALDO PARA EXIBIÇÃO 
-            formataValor(value) {
-                let val = (value/1).toFixed(2).replace('.', ',');
-                return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
             }
+        },
+        filters: {
+            formataValor: function (value) {
+                if (value) {
+                    let val = (value/1).toFixed(2).replace('.', ',');
+                    return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+                }
+            },
         }
     }
 </script>
